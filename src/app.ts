09719b62c0b9128c4ac6faa42b923b7f41b64e20
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2024-05-09 17:35:16 -0400

    User and session controller, services, and others

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import log from './utils/logger';
 import connect from './utils/mongoose';
 import routes from './routes';
 import config from 'config';
+import deserializeUser from './middleware/deserializeUser';
+
 
 const PORT = config.get<number>('port');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(deserializeUser);
 
 app.listen(PORT, async () => {
   log.info(`Server running on port ${PORT}`);
